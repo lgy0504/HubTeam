e92a03a5597b05940d592fc6a81d1931a4c45e4e
Hanchangsu <pok_jimi@naver.com>
2016-11-08 13:56:03 +0900

    Update MYJFrame.java
    로그인 UI 변경 (ActionListener 없음)

--- MYJFrame.java
+++ MYJFrame.java
@@ -6,28 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
+import javax.swing.border.EtchedBorder;
 import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JPasswordField;
-import javax.swing.JCheckBox;
-import java.awt.Color;
-import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.Font;
+import java.awt.Color;
 
-public class MYJFrame extends JFrame {
+public class LoginUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField ID_textField;
+	private JTextField PW_textField;
 
 	/**
 	 * Launch the application.
@@ -36,7 +28,7 @@ public class MYJFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MYJFrame frame = new MYJFrame();
+					LoginUI frame = new LoginUI();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -48,9 +40,10 @@ public class MYJFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MYJFrame() {
-		try{
-			UIManager.setLookAndFeel("com.jtattoo.plaf.mint.MintLookAndFeel");
+	public LoginUI() {
+		
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -65,54 +58,59 @@ public class MYJFrame extends JFrame {
 			e.printStackTrace();
 		}
 		
-		setTitle("Hub chatting program");
+		
+		setTitle("\uB028");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 285);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(133, 115, 190, 24);
-		contentPane.add(textField);
-		
-		JButton btnF = new JButton("\uB85C\uADF8\uC778");
-		btnF.setBounds(337, 114, 105, 27);
-		contentPane.add(btnF);
-		
-		JButton button = new JButton("\uD68C\uC6D0\uAC00\uC785");
-		button.setBounds(337, 153, 105, 27);
-		contentPane.add(button);
+		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel.setBounds(10, 10, 410, 230);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514");
-		lblNewLabel.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 17));
-		lblNewLabel.setBounds(57, 116, 62, 18);
-		contentPane.add(lblNewLabel);
+		lblNewLabel.setBounds(53, 100, 62, 18);
+		panel.add(lblNewLabel);
 		
-		JLabel label = new JLabel("\uBE44\uBC00\uBC88\uD638");
-		label.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 17));
-		label.setBounds(43, 155, 76, 18);
-		contentPane.add(label);
+		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
+		lblNewLabel_1.setBounds(46, 130, 62, 18);
+		panel.add(lblNewLabel_1);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(133, 154, 190, 24);
-		contentPane.add(passwordField);
+		ID_textField = new JTextField();
+		ID_textField.setBounds(120, 97, 116, 24);
+		panel.add(ID_textField);
+		ID_textField.setColumns(10);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("\uC544\uC774\uB514 \uC800\uC7A5");
-		chckbxNewCheckBox.setBounds(133, 183, 105, 27);
-		contentPane.add(chckbxNewCheckBox);
+		PW_textField = new JTextField();
+		PW_textField.setBounds(120, 127, 116, 24);
+		panel.add(PW_textField);
+		PW_textField.setColumns(10);
 		
-		JCheckBox checkBox = new JCheckBox("\uC790\uB3D9 \uB85C\uADF8\uC778");
-		checkBox.setBounds(244, 183, 103, 27);
-		contentPane.add(checkBox);
+		JButton Login_btn = new JButton("\uB85C\uADF8\uC778");
+		Login_btn.setBounds(252, 96, 90, 27);
+		panel.add(Login_btn);
 		
-		JLabel lblHub = new JLabel("Hub \uCC44\uD305\uD504\uB85C\uADF8\uB7A8");
-		lblHub.setBackground(Color.BLACK);
-		lblHub.setForeground(Color.PINK);
-		lblHub.setFont(new Font("¸¼Àº °íµñ", Font.BOLD | Font.ITALIC, 25));
-		lblHub.setBounds(133, 53, 220, 26);
-		contentPane.add(lblHub);
+		JButton New_btn = new JButton("\uD68C\uC6D0\uAC00\uC785");
+		New_btn.setBounds(252, 126, 90, 27);
+		panel.add(New_btn);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC11C\uBC84 \uC544\uC774\uD53C");
+		lblNewLabel_2.setBounds(53, 167, 80, 18);
+		panel.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("IP \uC785\uB825");
+		btnNewButton.setBounds(147, 163, 116, 27);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_3 = new JLabel("Hub Chatting Program");
+		lblNewLabel_3.setForeground(new Color(0, 0, 0));
+		lblNewLabel_3.setFont(new Font("Segoe Print", Font.BOLD, 25));
+		lblNewLabel_3.setBounds(53, 40, 301, 33);
+		panel.add(lblNewLabel_3);
 	}
 }
