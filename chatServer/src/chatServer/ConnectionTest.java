30e1001098a58f35beead2a91a5b8807d6687e12
Leejiyoung <dlwldud1995@gmail.com>
2016-11-09 19:37:04 +0900

    MySQL서버 생성 , 서버 연결 확인

---
+++ chatServer/src/chatServer/ConnectionTest.java
@@ -0,0 +1,35 @@
+package chatServer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+	public static void main(String[] args) {
+		try {
+			Connection con = null;
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost?useSSL=false",
+					"root", "0504lgy");
+
+			java.sql.Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("SHOW DATABASES");
+
+			if (st.execute("SHOW DATABASES")) {
+				rs = st.getResultSet();
+			}
+
+			while (rs.next()) {
+				String str = rs.getNString(1);
+				System.out.println(str);
+			}
+		} catch (SQLException sqex) {
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+		}
+
+	}
+}
