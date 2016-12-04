040b29639aff9cdd4fe7a62790d6571cc0f15c2d
KimHaYeong <hyk9607@daum.net>
2016-12-04 14:49:51 +0900

    IP UI 수정
    이미지 추가

--- src/InputIP.java
+++ src/InputIP.java
@@ -1,7 +1,11 @@
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,19 +14,23 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class InputIP extends JFrame {
 
 	 JTextField textField;
 	 JTextField textField_1;
-	 JLabel lblIpPort;
 	 JButton btninput;
 	 
 	public InputIP() {
 		
 		this.setTitle("Hub Chatting Program");
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+ 		Image img = toolkit.getImage("Image/Title.PNG");
+ 		setIconImage(img);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 365, 235);
+		this.setBounds(100, 100, 385, 235);
 		
 		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -30,42 +38,49 @@ public class InputIP extends JFrame {
 		this.setResizable(false);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
+		
+		final ImageIcon icon = new ImageIcon("image/ip&port.jpg");		//이미지 삽입
+		JPanel panel = new JPanel(){
+            public void paintComponent(Graphics g) {
+                g.drawImage(icon.getImage(), 0, 0, null);
+                setOpaque(false);
+                super.paintComponent(g);
+               } 
+        };
 		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		panel.setBounds(10, 10, 340, 180);
+		panel.setBounds(0, 0, 379, 207);
 		panel.setLayout(null);
 		contentPane.add(panel);
 		
 		JLabel IP = new JLabel("IP");
-		IP.setBounds(119, 70, 14, 21);
+		IP.setBounds(25, 104, 14, 21);
 		panel.add(IP);
+		//IP.setFont(new Font("굴림", Font.PLAIN, 18));
 		IP.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		textField = new JTextField();
-		textField.setBounds(138, 69, 116, 24);
+		textField.setBounds(115, 103, 116, 24);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		JLabel label = new JLabel("\uD3EC\uD2B8\uBC88\uD638");
-		label.setBounds(69, 105, 64, 19);
+		label.setBounds(25, 135, 64, 19);
 		panel.add(label);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setFont(new Font("굴림", Font.PLAIN, 16));
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(138, 103, 116, 24);
+		textField_1.setBounds(115, 134, 116, 24);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
-		lblIpPort = new JLabel("IP & Port number");
-		lblIpPort.setBounds(59, 12, 233, 45);
-		panel.add(lblIpPort);
-		lblIpPort.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIpPort.setForeground(Color.BLACK);
-		lblIpPort.setFont(new Font("Segoe Print", Font.BOLD, 25));
-		
-		btninput = new JButton("\uC785\uB825");
-		btninput.setBounds(138, 139, 61, 27);
+		btninput = new JButton("");
+		btninput.setIcon(new ImageIcon("image/Inputbtn.PNG"));
+		btninput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btninput.setBounds(139, 170, 61, 27);
 		panel.add(btninput);
 	}
 }
